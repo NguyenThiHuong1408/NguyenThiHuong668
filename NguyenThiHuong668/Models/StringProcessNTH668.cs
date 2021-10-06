c788a0b8b35b17bbf4ba3b213f1938923c208ba5
NguyenThiHuong1408 <huong26.nth@gmail.com>
2021-10-06 08:10:38 +0700

    Final_Commit_NTH668

--- NguyenThiHuong668/Models/StringProcessNTH668.cs
+++ NguyenThiHuong668/Models/StringProcessNTH668.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace NguyenThiHuong668.Models
 {
     public class StringProcessNTH668
     {
+    [Key]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        public string Rating { get; set; }
     }
 }
